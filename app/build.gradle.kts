303592ca841b456b13574b422ab36fb9ded2a2d3
NoahGarfunkel <noahsgarfunkel@gmail.com>
2023-12-12 20:53:37 -0500

    added secrets and started routing to make an authorization request (added ktor)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,13 +44,14 @@ android {
     }
     packaging {
         resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            excludes += "/META-INF/*"
         }
     }
 }
 
-dependencies {
+val ktor_version: String by project
 
+dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.0")
@@ -62,6 +63,10 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("io.ktor:ktor-client-core:$ktor_version")
+    implementation("io.ktor:ktor-client-cio:$ktor_version")
+    implementation("io.ktor:ktor-server-cio:$ktor_version")
+    implementation("io.ktor:ktor-server-netty:$ktor_version")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/musicsharing/LoginActivity.kt
+++ app/src/main/java/com/example/musicsharing/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.musicsharing
 
+import PropertiesReader
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -15,28 +16,44 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.musicsharing.ui.theme.MusicSharingTheme
 import androidx.compose.ui.Modifier
+import kotlinx.coroutines.*
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
 
 class LoginActivity : ComponentActivity() {
+    private lateinit var clientID: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent{
             MusicSharingTheme {
-                LoginScreen()
+                PropertiesReader.init(this)
+                clientID = PropertiesReader.getProperty("SPOTIFY_CLIENT_ID")
+                LoginScreen(clientID)
             }
         }
     }
 }
 
-@Preview
 @Composable
-fun LoginScreen(){
+fun LoginScreen(clientID: String){
     Column(
         modifier = Modifier.fillMaxWidth().fillMaxHeight(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         LoginButton(onClick = {
-
+                CoroutineScope(Dispatchers.Default).launch{
+                    authorization(clientID)
+                }
         })
     }
 }
@@ -49,4 +66,17 @@ fun LoginButton(onClick: () -> Unit) {
             color = MaterialTheme.colorScheme.primary
         )
     }
+}
+
+suspend fun authorization(clientID: String){
+    embeddedServer(Netty, host = "127.0.0.1", port = 8080) {
+        routing {
+            get("/callback") {
+                call.respondText("Hello, world!")
+            }
+        }
+    }.start(wait = true)
+    /*val client = HttpClient(CIO)
+    val response: HttpResponse = client.get("https://accounts.spotify.com/authorize?")
+    client.close()*/
 }
