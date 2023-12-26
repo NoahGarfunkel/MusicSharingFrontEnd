e8797900afdc36c9a7a9f289aa95a4ccdcb73253
NoahGarfunkel <noahsgarfunkel@gmail.com>
2023-12-26 14:40:00 -0500

    moving authorization to Login button, also refactoring the activity

--- app/src/main/java/com/example/musicsharing/LoginActivity.kt
+++ app/src/main/java/com/example/musicsharing/LoginActivity.kt
@@ -14,18 +14,11 @@ import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.musicsharing.ui.theme.MusicSharingTheme
 import androidx.compose.ui.Modifier
-import kotlinx.coroutines.*
-
-import com.spotify.android.appremote.api.ConnectionParams;
-import com.spotify.android.appremote.api.Connector;
-import com.spotify.android.appremote.api.SpotifyAppRemote;
-
-import com.spotify.protocol.client.Subscription;
-import com.spotify.protocol.types.PlayerState;
-import com.spotify.protocol.types.Track;
+import com.example.musicsharing.ui.theme.MusicSharingTheme
+import com.spotify.android.appremote.api.ConnectionParams
+import com.spotify.android.appremote.api.Connector
+import com.spotify.android.appremote.api.SpotifyAppRemote
 
 class LoginActivity : ComponentActivity() {
     private lateinit var clientID: String
@@ -36,15 +29,35 @@ class LoginActivity : ComponentActivity() {
 
         setContent{
             MusicSharingTheme {
-                PropertiesReader.init(this)
-                clientID = PropertiesReader.getProperty("SPOTIFY_CLIENT_ID")
-                LoginScreen(clientID)
+                LoginScreen()
             }
         }
     }
 
-    override fun onStart() {
-        super.onStart()
+    @Composable
+    fun LoginScreen(){
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            LoginButton(onClick = { authorization() })
+        }
+    }
+
+    @Composable
+    fun LoginButton(onClick: () -> Unit) {
+        OutlinedButton(onClick = { onClick() }) {
+            Text(
+                text = "Login with Spotify",
+                color = MaterialTheme.colorScheme.primary
+            )
+        }
+    }
+
+    private fun authorization(){
         PropertiesReader.init(this)
         clientID = PropertiesReader.getProperty("SPOTIFY_CLIENT_ID")
         val connectionParams = ConnectionParams.Builder(clientID)
@@ -56,52 +69,19 @@ class LoginActivity : ComponentActivity() {
             override fun onConnected(appRemote: SpotifyAppRemote) {
                 spotifyAppRemote = appRemote
                 Log.d("LoginActivity", "Connected! Yay!")
-                // Now you can start interacting with App Remote
-                connected()
             }
 
             override fun onFailure(throwable: Throwable) {
                 Log.e("LoginActivity", throwable.message, throwable)
-                // Something went wrong when attempting to connect! Handle errors here
             }
         })
     }
 
-    private fun connected() {
-        // Then we will write some more code here.
-    }
-
     override fun onStop() {
         super.onStop()
-        // Aaand we will finish off here.
+        spotifyAppRemote?.let {
+            SpotifyAppRemote.disconnect(it)
+        }
     }
 }
 
-@Composable
-fun LoginScreen(clientID: String){
-    Column(
-        modifier = Modifier.fillMaxWidth().fillMaxHeight(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        LoginButton(onClick = {
-                CoroutineScope(Dispatchers.Default).launch{
-                    authorization(clientID)
-                }
-        })
-    }
-}
-
-@Composable
-fun LoginButton(onClick: () -> Unit) {
-    OutlinedButton(onClick = { onClick() }) {
-        Text(
-            text = "Login with Spotify",
-            color = MaterialTheme.colorScheme.primary
-        )
-    }
-}
-
-suspend fun authorization(clientID: String){
-
-}
