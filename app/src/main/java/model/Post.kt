ec54e6c817a49a809ba8df5cbed68ef853075f0c
morganweltzer <weltzeme@mail.uc.edu>
2024-03-19 11:37:56 -0400

    cleaned up a little

--- app/src/main/java/model/Post.kt
+++ app/src/main/java/model/Post.kt
@@ -1,17 +1,5 @@
 package model
 
-//data class Post(
-//    val id: Int,
-//    val user: User,
-//    val hasImage: Boolean,
-//    val imageURL: String?,
-//    val numComments: Int,
-////    val numLikes: Int,
-//    val date: String,
-//    val text: String,
-//
-//    )
-
 data class Post(
     val content: String,
     val username: String,
