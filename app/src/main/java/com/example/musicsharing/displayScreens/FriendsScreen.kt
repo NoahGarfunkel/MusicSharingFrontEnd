98d098e4b2dcc86cb77a67ddfb644866ba2720ed
HouseAlex <houseah@mail.uc.edu>
2024-04-06 18:50:07 -0400

    rm import

--- app/src/main/java/com/example/musicsharing/displayScreens/FriendsScreen.kt
+++ app/src/main/java/com/example/musicsharing/displayScreens/FriendsScreen.kt
@@ -1,6 +1,5 @@
 package com.example.musicsharing.displayScreens
 
-import android.content.Context
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
